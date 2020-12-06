b040b477a57028410d399a870889e17106f216a1
flowersmichael <74375396+flowersmichael@users.noreply.github.com>
2020-12-06 00:30:53 -0800

    Delete PyPoll_block.py

--- PyPoll_block.py
+++
@@ -1,8 +0,0 @@
-   #Determine winning vote count and candidate.
-    #Determine if the votes are greater than the winning count.
-    if(votes > winning_count) and (vote_percentage>winning_percentage):
-        #If true then set winning_count = votes and winning_percentage = vote_percentage.
-        winning_count = votes
-        winning_percentage = vote_percentage
-        #And, set the winning_candidate equal to the candidate's name.
-        winning_candidate = candidate_name
